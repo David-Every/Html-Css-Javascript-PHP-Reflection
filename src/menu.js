f1830bb649da19f232958190d5da4d98206f42c2
David Every <David_every.ovr@hotmail.com>
2021-05-14 16:47:23 +0100

    Everything is working on site all that remains is sorting out plugins differances between eachother

--- src/menu.js
+++ src/menu.js
@@ -1,31 +1,31 @@
-const sideMenu = document.getElementById("sideMenu");
-//Controlling Main page content
-const main = document.getElementById("main");
-const over = document.getElementById("over");
+//||### Hamburger Menu Toggle ###||\\
+// document.getElementById("hamburger").addEventListener("click",ham);
+var element = document.getElementById("hamburger");
 
-over.style.display ="none";
-sideMenu.style.display ="none";
+$("#hamburger").on("click",activate);
+$('.site-overlay').on("click",deactivate);
 
-let open = false
 
-const bMenu = document.getElementById("menu").addEventListener('click',()=>{
-    if(!open){
-        open = true;
-        bAnim();
-        over.style.display ="block";
-        // over.style.marginRight = "350px";
-        // main.style.marginRight ="350px";
-        main.style.backgroundColor ="rgba(0,0,0,0.2)";
-        sideMenu.style.display ="block";
-    }
-    else{
-        open = false;
-        console.log("closing")
-        over.style.display ="none";
-        sideMenu.style.display ="none";
-    //     over.style.marginRight ="0";
-    //     main.style.marginRight = "0";
-    }
-    
-});
+function activate(){
+    element.classList.add("is-active");
+    // active = true;
+    $("#body,#html").css({"overflow-x":"hidden", "height":"100%", "-webkit-overflow-scrolling":"touch"});
+}
+function deactivate(){
+    element.classList.remove("is-active");
+    // active = false;
+    $("#body,#html").css({"overflow-x":"visible", "height":"100%"});
+}
 
+//||### /Hamburger Menu Toggle ###||\\
+
+
+
+//||### Pushy Additions||\\
+
+//Add the class "Pushy-link to all list elements in side menu"
+$('#pushy-cont ul li').addClass("pushy-link");
+
+/**
+ * I am trying to select  list 
+ */
